e776f5b7ba2353228c47e1b278d86cdde76c3422
Lucia <125606190+foundaxolotl@users.noreply.github.com>
2025-02-03 10:03:42 +1300

    video 2, temp heading

--- C_01_Temp_Gui_V1.py
+++ C_01_Temp_Gui_V1.py
@@ -15,6 +15,10 @@ class Converter():
         self.temp_frame = Frame(padx=10, pady=10)
         self.temp_frame.grid()
 
+        self.temp_heading = Label(self.temp_frame,
+                                  text="Temperature Converter")
+        self.temp_heading.grid(row=0)
+
 
 # main routine
 if __name__ == "__main__":
