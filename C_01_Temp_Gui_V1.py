47aa425f869eceb404db5b675b2bcd26d82d40c4
Lucia <125606190+foundaxolotl@users.noreply.github.com>
2025-02-03 09:56:48 +1300

    Video 2,  start of temp converter demo

---
+++ C_01_Temp_Gui_V1.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+
+class Converter():
+    """
+
+    Temperature conversion tool (°C, °F, °F to °C)
+    """
+    def __init__(self):
+        """
+        Temperature converter GUI
+        """
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
