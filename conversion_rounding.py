7f16b26a4eb0f0c5ffcef2a9b176321c04cc3e65
Lucia <125606190+foundaxolotl@users.noreply.github.com>
2025-02-10 13:22:10 +1300

    start of rounding

---
+++ conversion_rounding.py
@@ -0,0 +1,28 @@
+def round_ans(val):
+
+
+
+
+def to_celsius(to_convert):
+
+
+
+
+def to_fahrenheit(to_convert):
+
+
+
+
+# Main Routine / Testing starts here
+to_c_test = [0, 100, -459]
+to_f_test = [0, 100, 40, -273]
+
+for item in to_f_test:
+    ans = to_fahrenheit(item)
+    print(f"{item} C is {ans} F")
+
+print()
+
+for item in to_c_test:
+    ans = to_celsius(item)
+    print(f"{item} F is {ans} C")
