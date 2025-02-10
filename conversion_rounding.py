67ca6cbace1ae76fe7beca4a584b46cf13405e7b
Lucia <125606190+foundaxolotl@users.noreply.github.com>
2025-02-10 13:29:13 +1300

    Add files via upload

--- conversion_rounding.py
+++ conversion_rounding.py
@@ -1,16 +1,31 @@
 def round_ans(val):
-
-
+    """
+    Rounds temperatures to nearest degree
+    :param val: Number to be rounded
+    :return: Number rounded to nearest degree
+    """
+    var_rounded = (val * 2 + 1) // 2
+    return "{:.0f}".format(var_rounded)
 
 
 def to_celsius(to_convert):
-
-
+    """
+    Converts from F to C
+    :param to_convert: Temperature to be converted in F
+    :return: Converted temperature in C
+    """
+    answer = (to_convert - 32) * 5 / 9
+    return round_ans(answer)
 
 
 def to_fahrenheit(to_convert):
-
-
+    """
+    Converts from C to F
+    :param to_convert: Temperature to be converted in C
+    :return: Converted temperature in F
+    """
+    answer = to_convert * 1.8 + 32
+    return round_ans(answer)
 
 
 # Main Routine / Testing starts here
